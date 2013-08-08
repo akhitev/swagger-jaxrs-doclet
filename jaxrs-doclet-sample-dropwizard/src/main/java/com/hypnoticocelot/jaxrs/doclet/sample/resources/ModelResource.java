0e277298011c62ec7d44d92a015f21802ce6937e
Alexander Khitev <Alexander.khitev@gmail.com>
2013-08-08 22:52:52 +0400

    added put model parameter example - however not working on server side

--- jaxrs-doclet-sample-dropwizard/src/main/java/com/hypnoticocelot/jaxrs/doclet/sample/resources/ModelResource.java
+++ jaxrs-doclet-sample-dropwizard/src/main/java/com/hypnoticocelot/jaxrs/doclet/sample/resources/ModelResource.java
@@ -3,17 +3,21 @@ package com.hypnoticocelot.jaxrs.doclet.sample.resources;
 import com.hypnoticocelot.jaxrs.doclet.sample.api.ModelResourceModel;
 import org.joda.time.DateTime;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/ModelResource/{modelid}")
+@Path("/ModelResource")
 @Produces(MediaType.APPLICATION_JSON)
 public class ModelResource {
+
+    @Path("/{modelid}")
     @GET
     public ModelResourceModel getModel(@PathParam("modelid") long modelId) {
         return new ModelResourceModel(modelId, "Model Title", "Model Description", new DateTime());
     }
-}
+
+    @PUT
+    public ModelResourceModel addModel(ModelResourceModel model) {
+        return model;
+    }
+    }
